e56583c2f06c1c8ee08590f2cdbeedadf9b809e0
stefinracho <stefinracho@gmail.com>
2026-07-30 21:23:09 -0700

    Complete chapter 7

---
+++ ch07-packages-crates-and-modules/restaurant/src/lib.rs
@@ -0,0 +1,20 @@
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    // crate::front_of_house::hosting::add_to_waitlist();
+    // front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
