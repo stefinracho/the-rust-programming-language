4cf50bf85935bea22668fcd79aae30f27b0a2a51
stefinracho <stefinracho@gmail.com>
2026-08-05 12:30:44 -0700

    Complete chapter 9

--- ch09-error-handling/src/bin/02_recoverable_errors_with_result.rs
+++ ch09-error-handling/src/bin/02_recoverable_errors_with_result.rs
@@ -1,6 +1,9 @@
+use std::error::Error;
+use std::fs;
+use std::io::{self, Read};
 use std::{fs::File, io::ErrorKind};
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     // let greeting_file_result = File::open("hello.txt");
     // let greeting_file = match greeting_file_result {
     //     Ok(file) => file,
@@ -15,6 +18,44 @@ fn main() {
     //     },
     // };
     // let greeting_file = File::open("hello.txt").unwrap();
-    let greeting_file =
-        File::open("hello.txt").expect("hello.txt should be included in this project");
+    // let greeting_file =
+    //     File::open("hello.txt").expect("hello.txt should be included in this project");
+    //
+    let greeting_file = File::open("hello.txt")?;
+    Ok(())
+}
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let username_file_result = File::open("hello.txt");
+//
+//     let mut username_file = match username_file_result {
+//         Ok(file) => file,
+//         Err(e) => return Err(e),
+//     };
+//
+//     let mut username = String::new();
+//
+//     match username_file.read_to_string(&mut username) {
+//         Ok(_) => Ok(username),
+//         Err(e) => Err(e),
+//     }
+// }
+//
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let mut username_file = File::open("hello.txt")?;
+//     let mut username = String::new();
+//     username_file.read_to_string(&mut username)?;
+//     Ok(username)
+// }
+//
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let mut username = String::new();
+//
+//     File::open("hello.txt")?.read_to_string(&mut username)?;
+//
+//     Ok(username)
+// }
+//
+fn read_username_from_file() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
 }
