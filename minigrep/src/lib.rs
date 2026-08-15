4144c2c8f5c9a00833416201c43591759aa257fe
stefinracho <stefinracho@gmail.com>
2026-08-15 00:25:00 -0700

    Start learning about closures and iterators

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,30 +1,54 @@
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
+    // let mut results = Vec::new();
+    //
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(line);
+    //     }
+    // }
+    //
+    // results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
 
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+pub fn search_iter<'a>(query: &str, contents: &'a str) -> impl Iterator<Item = &'a str> {
+    contents.lines().filter(move |line| line.contains(query))
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    // let query = query.to_lowercase();
+    // let mut results = Vec::new();
+    //
+    // for line in contents.lines() {
+    //     if line.to_lowercase().contains(&query) {
+    //         results.push(line);
+    //     }
+    // }
+    //
+    // results
     let query = query.to_lowercase();
-    let mut results = Vec::new();
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
+}
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+pub fn search_case_insensitive_iter<'a>(
+    query: &str,
+    contents: &'a str,
+) -> impl Iterator<Item = &'a str> {
+    let query = query.to_lowercase();
+    contents
+        .lines()
+        .filter(move |line| line.to_lowercase().contains(&query))
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
     #[test]
@@ -40,6 +64,18 @@ Duct tape.";
     }
 
     #[test]
+    fn case_sensitive_iter() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert!(search_iter(query, contents).eq(["safe, fast, productive."].into_iter()))
+    }
+
+    #[test]
     fn case_insensitive() {
         let query = "rUsT";
         let contents = "\
@@ -52,5 +88,19 @@ Trust me.";
             vec!["Rust:", "Trust me."],
             search_case_insensitive(query, contents)
         );
+    }
+
+    #[test]
+    fn case_insensitive_iter() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert!(
+            search_case_insensitive_iter(query, contents).eq(["Rust:", "Trust me."].into_iter())
+        )
     }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,11 +1,16 @@
 use std::{env, error::Error, fs, process};
 
-use minigrep::{search, search_case_insensitive};
+use minigrep::{search, search_case_insensitive, search_case_insensitive_iter, search_iter};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    // let config = Config::build(&args).unwrap_or_else(|err| {
+    //     eprintln!("Problem parsing arguments: {err}");
+    //     process::exit(1);
+    // });
+
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
@@ -21,39 +26,64 @@ fn main() {
 
 pub struct Config {
     pub ignore_case: bool,
+    pub iter: bool,
     pub query: String,
     pub file_path: String,
 }
 
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    // fn build(args: &[String]) -> Result<Config, &'static str> {
+    //     if args.len() < 3 {
+    //         return Err("not enough arguments");
+    //     }
+    //
+    //     let ignore_case;
+    //     let query;
+    //     let file_path;
+    //
+    //     if args[1] == "-i" {
+    //         ignore_case = true;
+    //
+    //         if args.len() < 4 {
+    //             return Err("not enough arguments");
+    //         }
+    //
+    //         query = args[2].clone();
+    //         file_path = args[3].clone();
+    //     } else {
+    //         ignore_case = env::var("IGNORE_CASE").is_ok_and(|val| val == "1");
+    //         query = args[1].clone();
+    //         file_path = args[2].clone();
+    //     }
+    //
+    //     Ok(Config {
+    //         query,
+    //         file_path,
+    //         ignore_case,
+    //     })
+    // }
 
-        let ignore_case;
-        let query;
-        let file_path;
+    fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        let _program_name = args.next();
 
-        if args[1] == "-i" {
-            ignore_case = true;
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
 
-            if args.len() < 4 {
-                return Err("not enough arguments");
-            }
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
-            query = args[2].clone();
-            file_path = args[3].clone();
-        } else {
-            ignore_case = env::var("IGNORE_CASE").is_ok_and(|val| val == "1");
-            query = args[1].clone();
-            file_path = args[2].clone();
-        }
+        let ignore_case = env::var("IGNORE_CASE").is_ok_and(|val| val == "1");
+        let iter = env::var("ITER").is_ok_and(|val| val == "1");
 
         Ok(Config {
             query,
             file_path,
             ignore_case,
+            iter,
         })
     }
 }
@@ -61,14 +91,31 @@ impl Config {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    let results = if config.ignore_case {
-        search_case_insensitive(&config.query, &contents)
-    } else {
-        search(&config.query, &contents)
-    };
-
-    for line in results {
-        println!("{line}");
+    match config.ignore_case {
+        true => match config.iter {
+            true => {
+                for line in search_case_insensitive_iter(&config.query, &contents) {
+                    println!("{line}");
+                }
+            }
+            false => {
+                for line in search_case_insensitive(&config.query, &contents) {
+                    println!("{line}");
+                }
+            }
+        },
+        false => match config.iter {
+            true => {
+                for line in search_iter(&config.query, &contents) {
+                    println!("{line}");
+                }
+            }
+            false => {
+                for line in search(&config.query, &contents) {
+                    println!("{line}");
+                }
+            }
+        },
     }
 
     Ok(())
