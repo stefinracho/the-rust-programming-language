35778884f9d7bb0f634cb673e6ddf6ea9f04bda4
stefinracho <stefinracho@gmail.com>
2026-08-19 15:19:36 -0700

    Complete chapter 14

---
+++ ch14-more-about-cargo-and-crates-io/add/adder/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let num = 10;
+    println!("Hello, world! {num} plus one is {}!", add_one::add_one(num));
+    println!("Hello, world! {num} plus two is {}!", add_two::add_two(num));
+}
